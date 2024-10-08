1f27f2d2213948f44272c664c555334de5297b16
Samarth Pratap Singh <138753331+noviciusss@users.noreply.github.com>
2024-10-09 00:59:46 +0530

    Update Globe.tsx

--- components/Globe.tsx
+++ components/Globe.tsx
@@ -411,13 +411,6 @@ export function GlobeDemo() {
                     }}
                     className='div'
                 >
-                    <h2 className='text-center text-xl md:text-4xl font-bold text-black dark:text-white'>
-                        We sell soap worldwide
-                    </h2>
-                    <p className='text-center text-base md:text-lg font-normal text-neutral-700 dark:text-neutral-200 max-w-md mt-2 mx-auto'>
-                        This globe is interactive and customizable. Have fun with it, and don&apos;t
-                        forget to share it. :)
-                    </p>
                 </motion.div>
                 <div className='absolute w-full h-full z-10'>
                     <World
