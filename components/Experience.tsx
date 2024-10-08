b6e484de1889561c198b1d8a637921815a24d523
Samarth Pratap Singh <138753331+noviciusss@users.noreply.github.com>
2024-10-09 00:41:23 +0530

    Update Experience.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -31,7 +31,7 @@ const ExperienceTimeline = () => {
             </div>
             <div className='flex flex-col items-center mt-8'>
                 <h2 className='text-3xl font-bold mb-2 text-center'>Skills</h2>
-                <div className='w-64 h-1 bg-gradient-to-r from-gray-500 to-transparent mb-4'></div>
+                <div className='w-64 h-1 bg-gradient-to-r from-gray-500 to-transparent'></div>
                 <Skills />
             </div>
         </div>
