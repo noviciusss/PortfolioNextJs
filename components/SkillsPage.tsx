228e0d8214f2a858f49244415b5401baca31b566
Samarth Pratap Singh <138753331+noviciusss@users.noreply.github.com>
2024-10-15 10:06:03 +0530

    Update SkillsPage.tsx

--- components/SkillsPage.tsx
+++ components/SkillsPage.tsx
@@ -50,12 +50,12 @@ const projects = [
 
 const SkillsPage: React.FC = () => {
     return (
-        <div className="flex items-center justify-center min-h-screen p-4 m-4">
-            <div className="max-w-4xl w-full ">
-                <div className="mb-8">
+        <div className="flex items-center justify-center min-h-screen p-4 m-4 relative">
+            <div className="max-w-6xl w-full">
+                <div className="mb-12">
                     <h3 className='text-2xl font-semibold mb-2 text-gray-600'>My Work</h3>
                     <h1 className='text-6xl font-extrabold mb-4'>Projects.</h1>
-                    <p className="text-base text-gray-500 mx-auto max-w-2xl">
+                    <p className="text-base text-gray-500 mx-auto max-w-4xl mb-8">
                         Dive into my digital portfolio showcasing real-world projects that highlight my skills. 
                         Each entry offers a glimpse into my problem-solving approach, tech versatility, and project management chops. 
                         With quick descriptions and links to repos and demos, it's a snapshot of my ability to turn ideas into reality.
